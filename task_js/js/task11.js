7fe809a7a2867e9f549f1e0a500e182bda5cbdf1
555Message <zaku596@yahoo.co.jp>
2023-07-23 16:36:32 +0900

    test

---
+++ task_js/js/task11.js
@@ -0,0 +1,4 @@
+//課題3
+let e3 = document.getElementById('p3');
+
+e3.id="p5";
---
+++ task_js/js/task8.js
@@ -0,0 +1,7 @@
+let str = '12345';
+
+console.log(str + ':' + typeof(str));
+
+let n = parseInt(str);
+
+console.log(n + ' : ' + typeof(n));
