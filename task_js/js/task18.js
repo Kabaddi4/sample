559ffadbe0f0c22ec6d0169430064d3893a310ad
555Message <91714790+Kabaddi4@users.noreplay.github.com>
2023-08-14 19:01:06 +0900

    create task

---
+++ task_js/js/task18.js
@@ -0,0 +1,108 @@
+let itemList = [];
+
+let cate1 =[
+    '---',
+    '家具',
+    'ベッド・マットレス',
+    '収納家具・収納グッズ',
+    '子ども家具'
+];
+
+let cate2 = [
+    ['---'],
+    ['ベッド', 'ソファ', '棚・ラック', 'テーブル', '椅子'],
+    ['ベッド','寝具','マットレス'],
+    ['家具・ラック','収納システム'],
+    ['子ども部屋家具','ベビー家具・ベビーグッズ']
+];
+
+let cate1Element = document.getElementById('mainMenu');
+
+let cate2Element = document.getElementById('subMenu');
+
+let itemListElement = document.getElementById('itemList');
+
+function setMainMenu() {
+    //取得したselectを空白に
+    cate1Element.innerHTML = "";
+
+    for (let i = 0; i < cate1.length; i++) {
+        //option要素
+        let option = document.createElement('option');
+        option.value = cate[i];
+        option.text = cate[i];
+        cate1Element.appendChild(option);
+    }
+}
+
+function setSubMenu(idx) {
+    //取得したselectを空白に
+    cate2Element.innerHTML = "";
+
+    for (let i = 0; i < cate2[idx].length; i++) {
+        //option要素
+        let option = document.createElement('option');
+        option.value = cate2[idx][i];
+        option.text = cate2[idx][i];
+        cate2Element.appendChild(option);
+    }
+}
+
+function viewItemList(tag) {
+    console.log(tag);
+    let target = document.getElementById('itemList');
+
+    target.innerHTML = '';
+
+    if (tag !== undefined) {
+        let html = '';
+        html += '<table>';
+        for (let i = 0; i < itemList.length; i++) {
+            if (itemList[i].tags.some(t => t == tag)) {
+                if (count === 0) {
+                    html += '<tr>';
+                }
+
+                html += "<td>";
+                html += '<img src="img/item/sample.jpg" alt="商品の名前" width="180" height="123" />';
+                html += "<td>";
+
+                if (count == 5) {
+                    html += "</tr>";
+                    count = 0;
+                } else {
+                    count++;
+                }
+            }
+        }
+        if (count > 0) html += "</tr>";
+        html += "</table>";
+        target.innerHTML = html;
+    }
+}
+
+cate1Element.addEventListener('change', function(){   
+    let idx = cate1Element.selectedIndex;
+    setSubMenu(idx);
+    viewItemList(cate2[idx][0]);
+});
+
+cate2Element.addEventListener('change', function(){
+    let val = cate2Element.value;
+    viewItemList(val);
+});
+
+$(function () {
+    $.ajax({
+        url: 'json./item.json', //jsファイル読み込みと似てる。
+        dataType: 'json'
+    })
+    .done(function (data) {
+        itemList = data;
+
+        setMainMenu();
+    })
+    .fail(function () {
+        alert('ファイルが読み込めませんでした');
+    });
+});
---
+++ task_js/js/task20.js
@@ -0,0 +1,30 @@
+let app = new Vue({
+    el:'#app',             
+    data:{ msgs:["おはよう","こんばんは","お元気ですか"]}
+  });
+
+/*let initApp = {
+    el: '#app',
+    data : {
+        msg:'おはよう'
+    }
+};
+//インスタンス
+let app = new Vue(initApp); */
+
+/*  インスタンスのよくある書き方
+ let インスタンス = new Vue({
+    el: '#app'
+    data : {
+        msg:'おはよう'
+    }
+}); */
+
+/* イベンドハンドラ
+    methods: {
+        msgChange: function () {
+            this.msg1 = '値';
+            this.msg2 = '値';
+        }
+    }
+*/
