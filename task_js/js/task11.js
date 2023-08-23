9084830ba0308557a323264317424548318a74e8
555Message <91714790+Kabaddi4@users.noreplay.github.com>
2023-08-23 16:30:57 +0900

    create task

--- task_js/js/task11.js
+++ task_js/js/task11.js
@@ -1,4 +1,11 @@
+//課題1
+let e1 = document.getElementById('p1');
+e1.innerHTML="<input></input>";
+
+//課題２
+let e2 = document.getElementById('p2');
+e2.innerText="テキスト文字列";
+
 //課題3
 let e3 = document.getElementById('p3');
-
 e3.id="p5";
