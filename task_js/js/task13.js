6a0c125b0c14d6581e61591927192e6ff528373e
555Message <91714790+Kabaddi4@users.noreplay.github.com>
2023-08-28 17:52:29 +0900

    create task

--- task_js/js/task13.js
+++ task_js/js/task13.js
@@ -17,18 +17,18 @@ let cate2 = [
 ];
 
 var itemList = [
-    { id: '0001', name: 'ソファベッド', tags: ['ベッド', 'ソファ', '寝具']},
-    { id: '0002', name: 'シングルベッド', tags: ['ベッド', 'ソファ', '寝具']},
-    { id: '0003', name: '子ども用ベッド', tags: ['ベッド', 'ソファ', '寝具', '子ども部屋家具']},
-    { id: '0004', name: 'ソファ', tags: ['ソファ'] },
-    { id: '0005', name: 'キューブボックス', tags: ['家具・ラック', '収納システム'] },
-    { id: '0006', name: 'オープンラック', tags: ['家具・ラック', '収納システム'] },
-    { id: '0007', name: 'コンピュータデスク', tags: ['テーブル・椅子'] },
-    { id: '0008', name: 'サイドテーブル', tags: ['テーブル・椅子'] },
-    { id: '0009', name: 'ダイニングテーブル', tags: ['テーブル・椅子'] },
-    { id: '0010', name: '子ども部屋用収納', tags: ['収納システム', '子ども部屋家具'] },
-    { id: '0011', name: '調理道具', tags:['包丁', 'まな板',]},
-    { id: '0012', name: '調理機器', tags:['フライパン']}
+    { id: '0001', name: 'ソファベッド', tags: ['ベッド', 'ソファ', '寝具'], price: 500},
+    { id: '0002', name: 'シングルベッド', tags: ['ベッド', 'ソファ', '寝具'], price: 700},
+    { id: '0003', name: '子ども用ベッド', tags: ['ベッド', 'ソファ', '寝具', '子ども部屋家具'], price: 900},
+    { id: '0004', name: 'ソファ', tags: ['ソファ'], price: 1500 },
+    { id: '0005', name: 'キューブボックス', tags: ['家具・ラック', '収納システム'], price: 2500 },
+    { id: '0006', name: 'オープンラック', tags: ['家具・ラック', '収納システム'], price: 3300 },
+    { id: '0007', name: 'コンピュータデスク', tags: ['テーブル・椅子'], price: 4300 },
+    { id: '0008', name: 'サイドテーブル', tags: ['テーブル・椅子'], price: 4100 },
+    { id: '0009', name: 'ダイニングテーブル', tags: ['テーブル・椅子'], price: 5000 },
+    { id: '0010', name: '子ども部屋用収納', tags: ['収納システム', '子ども部屋家具'], price: 1000 },
+    { id: '0011', name: '調理道具', tags:['包丁', 'まな板',], price: 500 },
+    { id: '0012', name: '調理機器', tags:['フライパン'], price: 500 }
 ];
 
 let cate1Element = document.getElementById('mainMenu');
@@ -38,38 +38,36 @@ var itemListElement = document.getElementById('itemList');
 function setMainuMenu() {
     cate1Element.innerHTML = '';
 
-    for (let i = 0; i < cate1.length; i++) {
+    cate1.forEach(function(category) {
         let option = document.createElement('option');
-        option.value = cate1[i];
-        option.text = cate1[i];
+        option.value = category;
+        option.text = category;
         cate1Element.appendChild(option);
-    }
+    })
 }
-
 
 
 function setSubMenu(idx) {
     cate2Element.innerHTML = '';
 
-    for (let i = 0; i < cate2[1].length; i++) {
+    cate2[idx].forEach(function(item) {
         let option = document.createElement('option');
-        option.value = cate2[1][i];
-        option.text = cate2[1][i];
+        option.value = item;
+        option.text = item;
         cate2Element.appendChild(option);
-    }
+    })
 }
 
 function viewItemList(tag) {
     itemListElement.innerHTML = '';
-    for (let i = 0; i < itemList.length; i++) {
-        if (itemList[i].tags.some(t => t ===tag)) {
+    itemList.forEach(function(item) {
+        if (item.tags.some(t => t === tag)) {
             let li = document.createElement('li');
-            let text = document.createTextNode(itemList[i].id + ':' + 
-            itemList[i].name);
+            let text = document.createTextNode(item.id + ':' + item.name + ' ' + item.price + '円');
             li.appendChild(text);
             itemListElement.appendChild(li);
         }
-    }
+    });
 }
 
 cate1Element.addEventListener('change', function(){
